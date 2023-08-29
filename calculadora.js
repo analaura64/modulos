e8a8c981350c058bf01089824ac89a4ce40587b9
ana <analauraborgesmartins64@gmail.com>
2023-08-29 15:14:23 -0300

    ana

--- calculadora.js
+++ calculadora.js
@@ -1,5 +1,11 @@
+//ana laura
+
 var somar = require("./modulos/somar");
 var media = require("./modulos/media");
+var multiplicar = require("./modulos/multiplicar");
+var divisao = require("./modulos/divisao");
 
 console.log(somar(20,90));
 console.log(media(20,90));
+console.log(multiplicar(20,90));
+console.log(divisao(20,90));
