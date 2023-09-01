4204d4e26907758d42eefc3214655c144326b6e1
ana <analauraborgesmartins64@gmail.com>
2023-09-01 08:55:00 -0300

    mensagem

--- calculadora.js
+++ calculadora.js
@@ -4,8 +4,11 @@ var somar = require("./modulos/somar");
 var media = require("./modulos/media");
 var multiplicar = require("./modulos/multiplicar");
 var divisao = require("./modulos/divisao");
+const mensagem = require("./modulos/mensagem");
+var nome = "ana";
 
-console.log(somar(20,90));
-console.log(media(20,90));
-console.log(multiplicar(20,90));
-console.log(divisao(20,90));
+console.log("A soma é:" + somar(20,90));
+console.log("A média é:" + media(20,90));
+console.log("A multiplicação é:" + multiplicar(20,90));
+console.log("A divisão é:" + divisao(20,90));
+console.log(mensagem(nome));
---
+++ modulos/mensagem.js
@@ -0,0 +1,4 @@
+var mensagem= function (nome) {
+    return("Olá " + nome + ", seja bem vindo!")
+}
+module.exports=mensagem
